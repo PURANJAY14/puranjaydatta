45077c09ea07b23367bea717de7e6a42c14d25c8
PURANJAY14 <62548743+PURANJAY14@users.noreply.github.com>
2023-11-01 01:38:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
-function toggleTheme() { 
-    
-    var theme = document.getElementsByTagName('link')[0]; 
+function toggleTheme() {
+    const themeStyle = document.getElementById('theme-style');
+    const currentTheme = themeStyle.getAttribute('href');
 
-    if (theme.getAttribute('href') == 'stylesheet.css') { 
-        theme.setAttribute('href', 'nightmode.css'); 
-    } else { 
-        theme.setAttribute('href', 'stylesheet.css'); 
-    } 
-} 
+    if (currentTheme === 'stylesheet.css') {
+        themeStyle.setAttribute('href', 'nightmode.css');
+    } else if (currentTheme === 'nightmode.css') {
+        themeStyle.setAttribute('href', 'stylesheet.css');
+    }
+    //  // Update the class of the switch element to indicate the current theme.
+    //  themeStyle.classList.toggle('nightmode');
+}
+const themeButton = document.getElementById('theme-changer')
+themeButton.onclick = toggleTheme
+
+
+
